c27c7935bbe4e14ea42ab52be4e739edc6acf4e0
xrr2016 <13286904669@163.com>
2017-01-26 05:00:24 +0800

    :tada: first commit

---
+++ things.js
@@ -0,0 +1,32 @@
+const manyThings = [
+    function thing01() {
+        console.log('start 01')
+        setTimeout(() => {
+            console.log('Use 1000ms')
+        }, 1000)
+    },
+    function thing02() {
+        console.log('start 02')
+        setTimeout(() => {
+            console.log('Use 2000ms')
+        }, 2000)
+    },
+    function thing03() {
+        console.log('start 03')
+        setTimeout(() => {
+            console.log('Use 3000ms')
+        }, 3000)
+    },
+    function thing04() {
+        console.log('start 04')
+        setTimeout(() => {
+            console.log('Use 2500ms')
+        }, 2500)
+    },
+    function thing05() {
+        console.log('start 05')
+        setTimeout(() => {
+            console.log('Use 3500ms')
+        }, 3500)
+    }
+]
