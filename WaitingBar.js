46fbf39b7c739d1145930dc2cd5c8cb5a579dd46
xrr2016 <13286904669@163.com>
2017-01-26 18:00:06 +0800

    :fire: edit

--- WaitingBar.js
+++ WaitingBar.js
@@ -1,21 +1,31 @@
 (function(window, document) {
     const W = window,
-        D = document
+          D = document
     const primary = "#0275d8",
-        success = "#5cb85c",
-        info = "#5bc0de",
-        warning = "#f0ad4e",
-        danger = "#d9534f",
-        inverse = "#292b2c",
-        faded = "#f7f7f7"
-
+          success = "#5cb85c",
+          info = "#5bc0de",
+          warning = "#f0ad4e",
+          danger = "#d9534f",
+          inverse = "#292b2c",
+          faded = "#f7f7f7"
+    const options = {
+            color : 'lightblue',
+            height : 2,
+            direction : 'foward',
+            loacation : 'top',
+            timingFunction : 'ease',
+            shadow : true
+          }
     class WaitingBar {
-        constructor(tasks = [], color = 'lightblue', height = 4) {
-            this.height = height
-            this.tasks = tasks
+        constructor(tasks = [], options = options) {
+            this.options = options
+            if(this.options.color === 'random'){
+              this.options.color = this.getRandomColor()
+            }
+            this.tasks = [...tasks]
+            this.startTime = Date.now()
             this.animateTime = this.calculateTime(this.tasks) || 4000
-            this.color = color || this.getRandomColor()
-            this.bar = this.createLoadingBar(this.color, this.height)
+            this.bar = this.createLoadingBar(this.options.color, this.options.height)
             this.animation(this.bar)
         }
         next() {
@@ -39,41 +49,37 @@
             return color
         }
         calculateTime(tasks) {
-            const startTime = Date.now()
             while (tasks.length > 0) {
                 this.next()
                 if (tasks.length === 0) {
-                    return Date.now() - startTime
+                    return Date.now() - this.startTime
                 }
             }
         }
         animation(element) {
-            element.animate([{
-                    width: '0%'
-                },
-                {
-                    width: '100%'
-                }
+            element.animate([
+                { width: '0%'},
+                { width: '100%' }
             ], {
                 duration: 4000,
-                easing: "ease",
+                easing: this.options.timingFunction,
+                direction :this.options.direction,
                 iterations: Infinity
             })
         }
         getStyle(element) {
             element.style.position = 'absolute'
+            this.options.loacation === 'top' ? (element.style.top = 0 + 'px'):(element.style.bottom = 0+'px')
             element.style.left = 0 + 'px'
-            element.style.top = 0 + 'px'
             element.style.width = 0 + '%'
-            element.style.height = this.height + 'px'
-            element.style.backgroundColor = this.color
-            element.style.boxShadow = '0 2px 2px rgba(0,0,0,.2)'
+            element.style.height = this.options.height + 'px'
+            element.style.backgroundColor = this.options.color
+            element.style.boxShadow = this.options.shadow ? '0 2px 2px rgba(0,0,0,.2)' : null
             return element
         }
     }
-
-    function aWaitingBar(tasks, color, height) {
-        return new WaitingBar(tasks, color, height)
+    function aWaitingBar(tasks, options) {
+        return new WaitingBar(tasks,options)
     }
     W.aWaitingBar = aWaitingBar
     W.WaitingBar = WaitingBar
